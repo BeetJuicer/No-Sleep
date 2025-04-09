db384446e85ab482498b8426536659b26f7fc911
Carl Cap <94771147+BeetJuicer@users.noreply.github.com>
2025-04-09 17:47:33 +0800

    movement

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform respawnPoint;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private float respawnTime;
+
+    private float respawnTimeStart;
+
+    private bool respawn;
+
+    private CinemachineCamera CVC;
+
+    private void Start()
+    {
+        CVC = GameObject.Find("Player Camera").GetComponent<CinemachineCamera>();
+    }
+
+    private void Update()
+    {
+        CheckRespawn();
+    }
+    public void Respawn()
+    {
+        respawnTimeStart = Time.time;
+        respawn = true;
+    }
+
+    private void CheckRespawn()
+    {
+        if(Time.time >= respawnTimeStart + respawnTime && respawn)
+        {
+            var playerTemp = Instantiate(player, respawnPoint);
+            CVC.Follow = playerTemp.transform;
+            respawn = false;
+        }
+    }
+}
