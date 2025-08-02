7ed26555b92b6d3c8aac54da7f34586020dfbcff
BeetJuicer <cc.capilitan@gmail.com>
2025-08-02 23:32:20 +0800

    dirty reflection

---
+++ Assets/MirrorAnimation.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MirrorAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator original;
+    [SerializeField] private Transform originalTransform;
+    SpriteRenderer origRenderer;
+    SpriteRenderer myRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        origRenderer = originalTransform.GetComponent<SpriteRenderer>();
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (original.GetCurrentAnimatorStateInfo(0).IsName("PlayerIdleDown"))
+        {
+            GetComponent<Animator>().Play("PlayerIdleUp");
+        }
+
+        if (original.GetCurrentAnimatorStateInfo(0).IsName("PlayerIdleUp"))
+        {
+            GetComponent<Animator>().Play("PlayerIdleDown");
+        }
+
+        if (original.GetCurrentAnimatorStateInfo(0).IsName("PlayerWalkDown"))
+        {
+            GetComponent<Animator>().Play("PlayerWalkUp");
+        }
+
+
+        if (original.GetCurrentAnimatorStateInfo(0).IsName("PlayerWalkUp"))
+        {
+            GetComponent<Animator>().Play("PlayerWalkDown");
+        }
+
+        if (original.GetCurrentAnimatorStateInfo(0).IsName("PlayerWalkSide"))
+        {
+            GetComponent<Animator>().Play("PlayerWalkSide"); // set my animation clip as this
+        }
+
+        myRenderer.flipX = origRenderer.flipX;
+
+    }
+}
